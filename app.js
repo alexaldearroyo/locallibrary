0cb05cf10dac49a575bea429f7f21135d5b7d2fa
alexaldearroyo <alexaldearroyo@gmail.com>
2024-01-19 19:29:24 +0900

    App deployed in railway

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+const compression = require('compression');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -27,6 +32,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
+app.use(compression());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  max: 100 // limita cada IP a 100 solicitudes por ventana
+});
+
+app.use(limiter);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
